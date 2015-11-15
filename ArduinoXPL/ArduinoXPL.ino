c8442366064592169bbe267f3ffaa57191f66107
Jirka Masnik <jmasnik@gmail.com>
2015-11-15 20:33:05 +0100

    Nove cudly, druha obrazovka

--- ArduinoXPL/ArduinoXPL.ino
+++ ArduinoXPL/ArduinoXPL.ino
@@ -1,17 +1,7 @@
 #include <Wire.h>
-//#include <LCD.h>
 #include <LiquidCrystal_I2C.h>
-//#include <LiquidCrystal.h>
 
-#define I2C_ADDR    0x27 // <<----- Add your address here.  Find it from I2C Scanner
-#define BACKLIGHT_PIN     3
-#define En_pin  2
-#define Rw_pin  1
-#define Rs_pin  0
-#define D4_pin  4
-#define D5_pin  5
-#define D6_pin  6
-#define D7_pin  7
+#define I2C_ADDR    0x27
 
 #define ADF_LIMIT_LOW 190
 #define ADF_LIMIT_HIGH 535
@@ -31,7 +21,9 @@ LiquidCrystal_I2C	lcd(I2C_ADDR, 20, 4);
 uint8_t actual_screen;
 
 uint8_t sipka_left_pos;
+uint8_t sipka_left_dir;
 uint8_t sipka_right_pos;
+uint8_t sipka_right_dir;
 
 long cntr = 0;
 
@@ -43,19 +35,14 @@ int a_prev;
 int b_prev;
 int del = 0;
 
-int b1 = LOW;
-int b1_block = 0;
-
-int sel = 0;
-
 // hodnoty: 0 - hodnota nastavena tocitkem, 1 - hodnota LCD, 2 - hodnota poslana
 int val_hdg[3];
 int val_crs[3];
 int val_ad1[3];
 int val_ad2[3];
 
-char char_sipka[2] = {126,0};
-//char special_char[4] = {127,255,126,0};
+const char char_sipka_r[2] = {126,0};
+const char char_sipka_l[2] = {127,0};
 
 void setup(){
   int i;
@@ -66,14 +53,14 @@ void setup(){
   }
 
   // definice tlacitak
-  tlacitko_list[0].pin = 4;    tlacitko_list[0].block = 0;   // leve mackaci tocitko
-  tlacitko_list[1].pin = 5;    tlacitko_list[1].block = 0;   // leve dolni
-  tlacitko_list[2].pin = 6;    tlacitko_list[2].block = 0;   // leve horni
-  tlacitko_list[3].pin = 7;    tlacitko_list[3].block = 0;   // ?
-  tlacitko_list[4].pin = 8;    tlacitko_list[4].block = 0;   // ?
-  tlacitko_list[5].pin = 9;    tlacitko_list[5].block = 0;   // ?
-  tlacitko_list[6].pin = 10;   tlacitko_list[6].block = 0;   // ?
-  tlacitko_list[7].pin = 11;   tlacitko_list[7].block = 0;   // ?
+  tlacitko_list[0].pin = 4;    tlacitko_list[0].block = BUTTON_BLOCK_INT;   // leve mackaci tocitko
+  tlacitko_list[1].pin = 5;    tlacitko_list[1].block = BUTTON_BLOCK_INT;   // leve dolni
+  tlacitko_list[2].pin = 6;    tlacitko_list[2].block = BUTTON_BLOCK_INT;   // leve horni
+  tlacitko_list[3].pin = 7;    tlacitko_list[3].block = BUTTON_BLOCK_INT;   // horni levy
+  tlacitko_list[4].pin = 8;    tlacitko_list[4].block = BUTTON_BLOCK_INT;   // horni pravy
+  tlacitko_list[5].pin = 9;    tlacitko_list[5].block = BUTTON_BLOCK_INT;   // pravy dolni
+  tlacitko_list[6].pin = 10;   tlacitko_list[6].block = BUTTON_BLOCK_INT;   // prvay horni
+  tlacitko_list[7].pin = 11;   tlacitko_list[7].block = BUTTON_BLOCK_INT;   // pravy mackaci tocitko
 
   // nastaveni pinu
   for(i = 0; i < 8; i++){
@@ -88,32 +75,17 @@ void setup(){
 
   // aktualni obrazovka
   actual_screen = 1;
+
+  // sipky na radio obrazovce
   sipka_left_pos = 1;
   sipka_right_pos = 1;
+  sipka_left_dir = 1;
+  sipka_right_dir = 1;
 
   // LCD
   lcd.begin();  
   lcd.backlight();
 
-
-  lcd.setCursor (0,0);
-  lcd.print("HDG");
-  lcd.setCursor (0,1);
-  lcd.print("CRS");
-  lcd.setCursor (0,2);
-  lcd.print("AD1");
-  lcd.setCursor (0,3);
-  lcd.print("AD2");
-  
-  lcd.setCursor (9,0);
-  lcd.print("NAV1 120.55");
-  lcd.setCursor (9,1);
-  lcd.print("NAV2 150.55");
-  lcd.setCursor (9,2);
-  lcd.print("COM1 120.45");
-  lcd.setCursor (9,3);
-  lcd.print("COM2 140.63");
-  
   val_hdg[0] = 0;
   val_hdg[1] = 1;
   val_hdg[2] = 1;
@@ -130,7 +102,7 @@ void setup(){
   val_ad2[1] = 0;
   val_ad2[2] = 0;  
 
-  redraw1_sipka();
+  redraw1();
 }
 
 void loop() {
@@ -153,8 +125,29 @@ void loop() {
       // neco budeme delat - je to nove stisknuty
       tlacitko_list[i].block = BUTTON_BLOCK_INT;
 
+      if(i == 3){
+        if(actual_screen > 1){
+          actual_screen--;
+          switch_screen();
+        }
+      }
+      if(i == 4){
+        if(actual_screen < 2){
+          actual_screen++;
+          switch_screen();
+        }
+      }
+
       // radio stranka
       if(actual_screen == 1){
+        if(i == 0){
+          if(sipka_left_dir == 1){
+            sipka_left_dir = 2;
+          } else {
+            sipka_left_dir = 1;
+          }
+          redraw1_sipka();
+        }
         if(i == 1){
           if(sipka_left_pos < 4){
             sipka_left_pos++;
@@ -167,6 +160,26 @@ void loop() {
             redraw1_sipka();
           }
         }
+        if(i == 5){
+          if(sipka_right_pos < 4){
+            sipka_right_pos++;
+            redraw1_sipka();
+          }
+        }
+        if(i == 6){
+          if(sipka_right_pos > 1){
+            sipka_right_pos--;
+            redraw1_sipka();
+          }
+        }
+        if(i == 7){
+          if(sipka_right_dir == 1){
+            sipka_right_dir = 2;
+          } else {
+            sipka_right_dir = 1;
+          }
+          redraw1_sipka();
+        }        
       }
     }
     if(butt_state == LOW && tlacitko_list[i].block > 0){
@@ -182,7 +195,7 @@ void loop() {
       if(sipka_left_pos == 1){
         val_hdg[0]++;
         if(val_hdg[0] >= 360){
-          val_hdg[0] = 0;
+          val_hdg[0] = val_hdg[0] % 360;
         }
       }
       if(sipka_left_pos == 2){
@@ -293,26 +306,69 @@ void loop() {
 
 }
 
+void switch_screen(){
+  if(actual_screen == 1) redraw1();
+  if(actual_screen == 2) redraw2();
+}
+
+void redraw1(){
+  redraw1_static();
+  redraw1_sipka();
+}
+
 /**
  * Prekreslovani sipek na radio strance
  */
 void redraw1_sipka(){
     lcd.setCursor (3,0);
-    if(sipka_left_pos == 1) lcd.print(char_sipka); else lcd.print(" ");
+    if(sipka_left_pos == 1) lcd.print(sipka_left_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");
     lcd.setCursor (3,1);
-    if(sipka_left_pos == 2) lcd.print(char_sipka); else lcd.print(" ");    
+    if(sipka_left_pos == 2) lcd.print(sipka_left_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");    
     lcd.setCursor (3,2);
-    if(sipka_left_pos == 3) lcd.print(char_sipka); else lcd.print(" ");        
+    if(sipka_left_pos == 3) lcd.print(sipka_left_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");        
     lcd.setCursor (3,3);
-    if(sipka_left_pos == 4) lcd.print(char_sipka); else lcd.print(" ");  
+    if(sipka_left_pos == 4) lcd.print(sipka_left_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");  
 
     lcd.setCursor (13,0);
-    if(sipka_right_pos == 1) lcd.print(char_sipka); else lcd.print(" ");
+    if(sipka_right_pos == 1) lcd.print(sipka_right_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");
     lcd.setCursor (13,1);
-    if(sipka_right_pos == 2) lcd.print(char_sipka); else lcd.print(" ");    
+    if(sipka_right_pos == 2) lcd.print(sipka_right_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");    
     lcd.setCursor (13,2);
-    if(sipka_right_pos == 3) lcd.print(char_sipka); else lcd.print(" ");        
+    if(sipka_right_pos == 3) lcd.print(sipka_right_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");        
     lcd.setCursor (13,3);
-    if(sipka_right_pos == 4) lcd.print(char_sipka); else lcd.print(" ");      
+    if(sipka_right_pos == 4) lcd.print(sipka_right_dir == 1 ? char_sipka_r : char_sipka_l); else lcd.print(" ");
+}
+
+/**
+ * Vykresleni statickych veci na radio strance
+ */
+void redraw1_static(){
+  lcd.setCursor (0,0);
+  lcd.print("HDG      NAV1       ");
+  lcd.setCursor (0,1);
+  lcd.print("CRS      NAV2       ");
+  lcd.setCursor (0,2);
+  lcd.print("AD1      COM1       ");
+  lcd.setCursor (0,3);
+  lcd.print("AD2      COM2       ");
+}
+
+
+void redraw2(){
+  redraw2_static();
+}
+
+/**
+ * Staticka druha obrazovka
+ */
+void redraw2_static(){
+  lcd.setCursor (0,0);
+  lcd.print("SCREEN 2            ");
+  lcd.setCursor (0,1);
+  lcd.print("                    ");
+  lcd.setCursor (0,2);
+  lcd.print("                    ");
+  lcd.setCursor (0,3);
+  lcd.print("                    ");
 }
 
