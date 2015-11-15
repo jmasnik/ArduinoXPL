00cf9828fcfb773766500942092e0a73a95d805d
Jirka Masnik <jmasnik@gmail.com>
2015-11-15 12:01:28 +0100

    tlacitka, sipky

--- ArduinoXPL/ArduinoXPL.ino
+++ ArduinoXPL/ArduinoXPL.ino
@@ -16,7 +16,22 @@
 #define ADF_LIMIT_LOW 190
 #define ADF_LIMIT_HIGH 535
 
+#define BUTTON_BLOCK_INT 500
+
+typedef struct {
+  uint8_t pin;
+  uint16_t block;
+} tlacitko;
+
+tlacitko tlacitko_list[8];
+
 LiquidCrystal_I2C	lcd(I2C_ADDR, 20, 4);
+
+// aktualni zobrazovana obrazovka (1=radia)
+uint8_t actual_screen;
+
+uint8_t sipka_left_pos;
+uint8_t sipka_right_pos;
 
 long cntr = 0;
 
@@ -43,20 +58,43 @@ char char_sipka[2] = {126,0};
 //char special_char[4] = {127,255,126,0};
 
 void setup(){
+  int i;
+
+  // seriak
   Serial.begin(9600);
   while (!Serial) {
   }
-  
+
+  // definice tlacitak
+  tlacitko_list[0].pin = 4;    tlacitko_list[0].block = 0;   // leve mackaci tocitko
+  tlacitko_list[1].pin = 5;    tlacitko_list[1].block = 0;   // leve dolni
+  tlacitko_list[2].pin = 6;    tlacitko_list[2].block = 0;   // leve horni
+  tlacitko_list[3].pin = 7;    tlacitko_list[3].block = 0;   // ?
+  tlacitko_list[4].pin = 8;    tlacitko_list[4].block = 0;   // ?
+  tlacitko_list[5].pin = 9;    tlacitko_list[5].block = 0;   // ?
+  tlacitko_list[6].pin = 10;   tlacitko_list[6].block = 0;   // ?
+  tlacitko_list[7].pin = 11;   tlacitko_list[7].block = 0;   // ?
+
+  // nastaveni pinu
+  for(i = 0; i < 8; i++){
+    pinMode(tlacitko_list[i].pin ,INPUT_PULLUP);  
+  }
+
+  // piny k rotacnim
   pinMode(2,INPUT_PULLUP);
   pinMode(3,INPUT_PULLUP);
-  pinMode(4,INPUT_PULLUP);
-  
-  lcd.begin(); //  <<----- My LCD was 16x2
-  
-  // Switch on the backlight
-  //lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
-  //lcd.setBacklight(HIGH);
+  pinMode(12,INPUT_PULLUP);
+  pinMode(13,INPUT_PULLUP);
+
+  // aktualni obrazovka
+  actual_screen = 1;
+  sipka_left_pos = 1;
+  sipka_right_pos = 1;
+
+  // LCD
+  lcd.begin();  
   lcd.backlight();
+
 
   lcd.setCursor (0,0);
   lcd.print("HDG");
@@ -76,10 +114,6 @@ void setup(){
   lcd.setCursor (9,3);
   lcd.print("COM2 140.63");
   
-  //lcd.print("Vsechno nejlepsi ;)");
-  //
-  //lcd.print(ch);
-  
   val_hdg[0] = 0;
   val_hdg[1] = 1;
   val_hdg[2] = 1;
@@ -95,9 +129,14 @@ void setup(){
   val_ad2[0] = 448;
   val_ad2[1] = 0;
   val_ad2[2] = 0;  
+
+  redraw1_sipka();
 }
 
 void loop() {
+  int i;
+  int butt_state;
+  
   del++;
   
   a_prev = a;
@@ -105,77 +144,88 @@ void loop() {
   
   a = digitalRead(2);
   b = digitalRead(3);
-  b1 = digitalRead(4);  
 
-  // poreseni cudlu
-  if(b1 == HIGH && b1_block == 0){
-    sel++;
-    if(sel > 3){
-      sel = 0;
+  // tlacitka
+  for(i = 0; i < 8; i++){
+    // nacteni stavu
+    butt_state = digitalRead(tlacitko_list[i].pin);
+    if(butt_state == HIGH && tlacitko_list[i].block == 0){
+      // neco budeme delat - je to nove stisknuty
+      tlacitko_list[i].block = BUTTON_BLOCK_INT;
+
+      // radio stranka
+      if(actual_screen == 1){
+        if(i == 1){
+          if(sipka_left_pos < 4){
+            sipka_left_pos++;
+            redraw1_sipka();
+          }
+        }
+        if(i == 2){
+          if(sipka_left_pos > 1){
+            sipka_left_pos--;
+            redraw1_sipka();
+          }
+        }
+      }
     }
-    b1_block = 500;
-    
-    lcd.setCursor (3,0);
-    if(sel == 0) lcd.print(char_sipka); else lcd.print(" ");
-    lcd.setCursor (3,1);
-    if(sel == 1) lcd.print(char_sipka); else lcd.print(" ");    
-    lcd.setCursor (3,2);
-    if(sel == 2) lcd.print(char_sipka); else lcd.print(" ");        
-    lcd.setCursor (3,3);
-    if(sel == 3) lcd.print(char_sipka); else lcd.print(" ");            
-  }
-  if(b1 == LOW && b1_block > 0){
-    b1_block--;
+    if(butt_state == LOW && tlacitko_list[i].block > 0){
+      tlacitko_list[i].block--;
+    }
   }
 
   // poreseni tocitka
   if(a_prev == LOW && b_prev == LOW){
+
+    // zvysovani
     if(a == LOW && b == HIGH){
-      if(sel == 0){
+      if(sipka_left_pos == 1){
         val_hdg[0]++;
         if(val_hdg[0] >= 360){
           val_hdg[0] = 0;
         }
       }
-      if(sel == 1){
+      if(sipka_left_pos == 2){
         val_crs[0]++;
         if(val_crs[0] >= 360){
           val_crs[0] = 0;
         }
       }      
-      if(sel == 2){
+      if(sipka_left_pos == 3){
         val_ad1[0]++;
         if(val_ad1[0] > ADF_LIMIT_HIGH){
           val_ad1[0] = ADF_LIMIT_LOW;
         }
       }            
-      if(sel == 3){
+      if(sipka_left_pos == 4){
         val_ad2[0]++;
         if(val_ad2[0] > ADF_LIMIT_HIGH){
           val_ad2[0] = ADF_LIMIT_LOW;
         }
       }
     }
+
+    // snizovani
     if(a == HIGH && b == LOW){
-      if(sel == 0){      
+      if(sipka_left_pos == 1){      
         val_hdg[0]--;
         if(val_hdg[0] < 0){
           val_hdg[0] = 359;
         }
       }      
-      if(sel == 1){      
+      if(sipka_left_pos == 2){      
         val_crs[0]--;
         if(val_crs[0] < 0){
           val_crs[0] = 359;
         }
       }
-      if(sel == 2){
+      if(sipka_left_pos == 3){
         val_ad1[0]--;
         if(val_ad1[0] < ADF_LIMIT_LOW){
           val_ad1[0] = ADF_LIMIT_HIGH;
         }
       }
-      if(sel == 3){
+      if(sipka_left_pos == 4){
         val_ad2[0]--;
         if(val_ad2[0] < ADF_LIMIT_LOW){
           val_ad2[0] = ADF_LIMIT_HIGH;
@@ -242,3 +292,27 @@ void loop() {
   }
 
 }
+
+/**
+ * Prekreslovani sipek na radio strance
+ */
+void redraw1_sipka(){
+    lcd.setCursor (3,0);
+    if(sipka_left_pos == 1) lcd.print(char_sipka); else lcd.print(" ");
+    lcd.setCursor (3,1);
+    if(sipka_left_pos == 2) lcd.print(char_sipka); else lcd.print(" ");    
+    lcd.setCursor (3,2);
+    if(sipka_left_pos == 3) lcd.print(char_sipka); else lcd.print(" ");        
+    lcd.setCursor (3,3);
+    if(sipka_left_pos == 4) lcd.print(char_sipka); else lcd.print(" ");  
+
+    lcd.setCursor (13,0);
+    if(sipka_right_pos == 1) lcd.print(char_sipka); else lcd.print(" ");
+    lcd.setCursor (13,1);
+    if(sipka_right_pos == 2) lcd.print(char_sipka); else lcd.print(" ");    
+    lcd.setCursor (13,2);
+    if(sipka_right_pos == 3) lcd.print(char_sipka); else lcd.print(" ");        
+    lcd.setCursor (13,3);
+    if(sipka_right_pos == 4) lcd.print(char_sipka); else lcd.print(" ");      
+}
+
